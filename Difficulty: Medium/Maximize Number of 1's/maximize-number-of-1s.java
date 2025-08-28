1ae128a25532424fe664ec4fe9335b6e6dde09c4
PIYUSHA BHADANGE. <116305709+piyushabhadange09@users.noreply.github.com>
2025-08-28 17:21:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxOnes(int arr[], int k) {
+        // code here
+        int left = 0, maxLen = 0, zeros = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            // Count zero if found
+            if (arr[right] == 0) {
+                zeros++;
+            }
+
+            
+            while (zeros > k) {
+                if (arr[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+
+           
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
