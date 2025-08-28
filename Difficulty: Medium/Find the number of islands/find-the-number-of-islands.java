fb58c99111a9d60ac2a754cd6ff632952ca59b64
PIYUSHA BHADANGE. <116305709+piyushabhadange09@users.noreply.github.com>
2025-08-28 17:20:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,61 +1,47 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int tc = scanner.nextInt();
-        while (tc-- > 0) {
-            int n = scanner.nextInt();
-            int m = scanner.nextInt();
-            char[][] grid = new char[n][m];
-
-            // Read the grid input
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = scanner.next().charAt(0);
+class Solution {
+     static class Pair {
+        int first, second;
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    private static void bfs(int row,int col,int vis[][],char grid[][]){
+        vis[row][col]=1;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(row,col));
+        int n=grid.length;
+        int m=grid[0].length;
+        while(!q.isEmpty()){
+            int ro=q.peek().first;
+            int co=q.peek().second;
+            q.remove();
+            for(int delRow=-1;delRow<=1;delRow++){
+                for(int delCol=-1;delCol<=1;delCol++){
+                    int nrow=ro+delRow;
+                    int ncol=co+delCol;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]=='L'){
+                        vis[nrow][ncol]=1;
+                        bfs(nrow,ncol,vis,grid);
+                    }
                 }
             }
-            Solution obj = new Solution();
-            int ans = obj.countIslands(grid);
-            System.out.println(ans);
-            System.out.println("~");
         }
-        scanner.close();
     }
-}
-
-// } Driver Code Ends
-
-
-class Solution {
     public int countIslands(char[][] grid) {
         // Code here
-          int n=grid.length;
+        int n=grid.length;
         int m=grid[0].length;
+        int vis[][]=new int[n][m];
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='L'){
-                    dfs(grid,i,j,n,m);
+                if(vis[i][j]==0 && grid[i][j]=='L'){
                     count++;
+                    bfs(i,j,vis,grid);
                 }
             }
         }
         return count;
-    }
-       public static void dfs(char[][] grid,int i,int j,int n,int m){
-         
-        grid[i][j]='K';
-        for(int r=-1;r<=1;r++){
-            for(int c=-1;c<=1;c++){
-                int nr=i+r;
-                int nc=j+c;
-                if(nr>=0 && nr<n && nc<m && nc>=0 && grid[nr][nc]=='L'){
-                   dfs(grid,nr,nc,n,m);
-                }
-            }
-        }
-        
-    }
+    }   
 }
